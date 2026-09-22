a2ba11217c905a916e22075c17593ee1b57f63da
Gisela <gandradem@estudiante.uniajc.edu.co>
2026-09-21 20:52:04 -0500

    Landing page peluches

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.navbar a').forEach(link => {
+
+    link.addEventListener('click', function(event) {
+
+        event.preventDefault();
+
+        const destino = document.querySelector(
+            this.getAttribute('href')
+        );
+
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
